7f6444b0615c1792268f5cd21b95a23c47dce069
frets1700 <panmol1700@gmail.com>
2018-08-24 17:13:26 -0400

    adding ngrok script

---
+++ scripts/ngrok.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing ngrok"
+
+# check if we have authentication set
+if [[ -z $1 ]]; then
+    echo ">>> ngrok needs auth token , please create an account in ngrok and get the auth token"
+else
+    AUTH_TOKEN=$1
+    cd /opt
+    sudo mkdir ngrok && cd ngrok
+    sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    sudo unzip ngrok-stable-linux-amd64.zip
+    sudo chmod +x ngrok
+    sudo mv ngrok /usr/local/bin/ngrok
+    sudo touch /home/vagrant/.ngrok2/ngrok.conf
+    sudo echo "web_addr: 0.0.0.0:4040" | sudo tee -a /home/vagrant/.ngrok2/ngrok.conf
+    sudo echo "authtoken: $1" | sudo tee -a /home/vagrant/.ngrok2/ngrok.conf
+
+    # Run using
+    # ngrok http -config=/home/vagrant/.ngrok2/ngrok.yml 80
+fi
