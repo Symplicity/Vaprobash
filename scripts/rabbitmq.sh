4ee380d8249435c3b63b130af35b1c59e28c1ea1
frets1700 <panmol1700@gmail.com>
2021-10-08 10:20:17 -0400

    fix erlang rabbitmq update

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -5,22 +5,24 @@ if [[ $VERBOSE != true ]]; then
     exec >/dev/null 2>&1
 fi
 
-echo ">>> Installing RabbitMQ"
+package=rabbitmq-server
+RABBITMQ_IS_INSTALLED="$(dpkg-query -W --showformat='${db:Status-Status}' "$package" 2>&1)"
+if [ ! $? = 0 ] || [ ! "$RABBITMQ_IS_INSTALLED" = installed ]; then
+    echo ">>> Installing RabbitMQ"
+    apt-get -y install erlang-nox
+    wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+    apt-get update
+    apt-get install -y rabbitmq-server
+    rabbitmq-plugins enable rabbitmq_management
 
-apt-get -y install erlang-nox
-wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-apt-get update
-apt-get install -y rabbitmq-server
-rabbitmq-plugins enable rabbitmq_management
+    # Allow guests to login thru rabbitmq management
+    if ! grep -q "loopback_users" "/etc/rabbitmq/rabbitmq.config"; then
+        echo "[{rabbit, [{loopback_users, []}]}]." >> /etc/rabbitmq/rabbitmq.config
+    fi
 
-# Allow guests to login thru rabbitmq management
-if ! grep -q "loopback_users" "/etc/rabbitmq/rabbitmq.config"; then
-	echo "[{rabbit, [{loopback_users, []}]}]." >> /etc/rabbitmq/rabbitmq.config
+    service rabbitmq-server restart
+    rabbitmqctl add_user $1 $2
+    rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
+else
+    echo ">>> RabbitMQ Server is already installed. Skipping installation"
 fi
-
-service rabbitmq-server restart
-
-rabbitmqctl add_user $1 $2
-rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
