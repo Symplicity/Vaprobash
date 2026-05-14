63879390cd2fa8c7e160f48fea90371da0622a3d
frets1700 <panmol1700@gmail.com>
2026-05-14 06:27:50 -0400

    fix kibana setup for access

--- scripts/kibana.sh
+++ scripts/kibana.sh
@@ -15,4 +15,9 @@ echo ">>> Installing Kibana $KIBANA_VERSION"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install -qq kibana=$1
+
+if ! sudo grep -Eq '^[[:space:]]*server\.host:' /etc/kibana/kibana.yml; then
+    echo 'server.host: "0.0.0.0"' | sudo tee -a /etc/kibana/kibana.yml
+fi
+
 sudo systemctl restart kibana
